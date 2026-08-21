ee93576f3199a82440ab5cd4b1d7e4c1f7ff228a
thinh204 <huythinh592@gmail.com>
2026-08-21 23:19:18 +0700

    commit first

---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main className="min-h-screen">
+      <h1 className="text-3xl font-bold text-center mt-10">StayEase - Coming Soon</h1>
+    </main>
+  );
+}
